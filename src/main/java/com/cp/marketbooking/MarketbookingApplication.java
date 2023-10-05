0166a07196a9b2ac11dca62d99699db44a092092
BasTaks <taksin.cup@gmail.com>
2023-10-05 21:33:49 +0700

    add index page and weather api

--- src/main/java/com/cp/marketbooking/MarketbookingApplication.java
+++ src/main/java/com/cp/marketbooking/MarketbookingApplication.java
@@ -2,8 +2,10 @@ package com.cp.marketbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// @ComponentScan (basePackages = {"com.cp.marketbooking.Controller.MainController"})
 public class MarketbookingApplication {
 
 	public static void main(String[] args) {
