54f5361c85d27312c9ee3c41d61e7fd695bd583e
BasTaks <taksin.cup@gmail.com>
2023-10-05 16:34:11 +0700

    create project

---
+++ src/main/java/com/cp/marketbooking/MarketbookingApplication.java
@@ -0,0 +1,13 @@
+package com.cp.marketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketbookingApplication.class, args);
+	}
+
+}
